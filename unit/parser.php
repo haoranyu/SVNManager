b28c6a8694e8ba28ae8d7e4262afcd27241e8624
haoranyu <haoran_yu@hotmail.com>
2015-03-18 23:04:31 -0500

    Add amaze UI as front framework

--- unit/parser.php
+++ unit/parser.php
@@ -1,5 +1,8 @@
 <?php
-    $parser = Parser::parse('./data/svn_log.xml', true);
-    Test::assertEqual('Test parser 1', $parser['logentry'][0]['author'], 'hyu34' );
-    Test::assertEqual('Test parser 2', $parser['logentry'][1]['@attributes'], array('revision' => '4124') );
-    //print_r($parser);
+    $svnLog = Parser::parse('./resource/data/svn_log.xml', true);
+    $svnList = Parser::parse('./resource/data/svn_list.xml', true);
+    Test::assertEqual('Test parser for log 1', $svnLog['logentry'][0]['author'], 'hyu34' );
+    Test::assertEqual('Test parser for log 2', $svnLog['logentry'][1]['@attributes'], array('revision' => '4124') );
+    Test::assertEqual('Test parser for list 1', $svnList['list']['@attributes']['path'], 'https://subversion.ews.illinois.edu/svn/sp15-cs242/hyu34' );
+    Test::assertEqual('Test parser for list 2', $svnList['list']['entry'][0]['commit']['author'], 'hyu34' );
+//    print_r($svnList);
